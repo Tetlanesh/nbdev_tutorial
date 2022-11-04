75e68d7483f31ccdff8594a6fd9724342b62136f
Tetlanesh <marcin.strus@gmail.com>
2022-11-04 12:44:25 +0100

    test soime changes

--- nbdev_tutorial/_modidx.py
+++ nbdev_tutorial/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://Tetlanesh.github.io',
                 'git_url': 'https://github.com/Tetlanesh/nbdev_tutorial',
                 'lib_path': 'nbdev_tutorial'},
-  'syms': {'nbdev_tutorial.core': {'nbdev_tutorial.core.foo': ('core.html#foo', 'nbdev_tutorial/core.py')}}}
+  'syms': { 'nbdev_tutorial.core': { 'nbdev_tutorial.core.foo': ('core.html#foo', 'nbdev_tutorial/core.py'),
+                                     'nbdev_tutorial.core.foo2': ('core.html#foo2', 'nbdev_tutorial/core.py')}}}
